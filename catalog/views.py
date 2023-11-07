3e20fe61b5bb9e81f673a3a144b6d7de18af1a14
Anna Raketa <rocketvsaliens@gmail.com>
2023-11-07 21:21:59 +0300

    Initial commit

---
+++ catalog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'catalog/index.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'{name} ({email}): {message}')
+    return render(request, "catalog/contacts.html")

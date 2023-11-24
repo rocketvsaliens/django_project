b31c1228408493dee9c6ea7e225950a573624b15
Anna Raketa <rocketvsaliens@gmail.com>
2023-11-24 12:17:53 +0300

    Update filldb command

--- catalog/management/commands/filldb.py
+++ catalog/management/commands/filldb.py
@@ -14,12 +14,16 @@ class Command(BaseCommand):
             {'name': 'Протезы и экзоскелеты', 'description': 'Tomsk Technologies'},
         ]
 
+        cat1 = categories_list[0].get('name')
+        cat2 = categories_list[1].get('name')
+        cat3 = categories_list[2].get('name')
+
         product_list = [
-            {'name': 'Ваня', 'description': 'Робот-пылесос', 'category_id': 1, 'price': '0'},
-            {'name': 'Глаша', 'description': 'Робот-доярка', 'category_id': 1, 'price': '10000'},
-            {'name': 'Гошик', 'description': 'Робот-массажист', 'category_id': 1, 'price': '10000'},
-            {'name': 'Галя', 'description': 'Голограмма жены по подписке', 'category_id': 2, 'price': '100500'},
-            {'name': 'Роборука', 'description': 'Ворованная', 'category_id': 3, 'price': '100'}
+            {'name': 'Ваня', 'description': 'Робот-пылесос', 'category': cat1, 'price': '0'},
+            {'name': 'Глаша', 'description': 'Робот-доярка', 'category': cat1, 'price': '10000'},
+            {'name': 'Гошик', 'description': 'Робот-массажист', 'category': cat1, 'price': '10000'},
+            {'name': 'Галя', 'description': 'Голограмма жены по подписке', 'category': cat2, 'price': '100500'},
+            {'name': 'Роборука', 'description': 'Ворованная', 'category': cat3, 'price': '100'}
         ]
 
         for category_item in categories_list:
@@ -27,8 +31,8 @@ class Command(BaseCommand):
 
         products_for_create = []
         for product_item in product_list:
-            category_id = product_item.pop('category_id')
-            category = Category.objects.get(id=category_id)
+            category = product_item.pop('category')
+            category = Category.objects.get(name=category)
             product_item['category'] = category
             products_for_create.append(
                 Product(**product_item)

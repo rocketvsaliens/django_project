67bf868de1ae639d339ce3e57f8275751aca7792
Anna Raketa <rocketvsaliens@gmail.com>
2023-12-21 16:52:28 +0300

    Homework 22.1 task 1

---
+++ catalog/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+
+from catalog.models import Product
+
+
+class ProductForm(forms.ModelForm):
+    RESTRICTED_WORDS = [
+        'казино', 'криптовалюта', 'крипта', 'биржа', 'дешево',
+        'дёшево', 'бесплатно', 'обман', 'полиция', 'радар'
+    ]
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, forms.ChoiceField):
+                field.widget.attrs['class'] = 'form-select'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
+    def restrict_words(self, title):
+        for word in self.RESTRICTED_WORDS:
+            if word.lower() in title.lower():
+                raise forms.ValidationError(f'Недопустимое слово "{word}"')
+
+    def clean_name(self):
+        cleaned_data = (self.cleaned_data.get('name'))
+        self.restrict_words(cleaned_data)
+        return cleaned_data
+
+    def clean_description(self):
+        cleaned_data = self.cleaned_data.get('description')
+        self.restrict_words(cleaned_data)
+        return cleaned_data
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,7 @@
-from django.views.generic import ListView, DetailView
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from catalog.forms import ProductForm
 from catalog.models import Product, Contact
 
 
@@ -6,6 +9,7 @@ class ProductListView(ListView):
     paginate_by = 5
     model = Product
     template_name = 'catalog/index.html'
+    ordering = ['-pk']
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -21,6 +25,30 @@ class ProductDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['title'] = str(context['object'])
         return context
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:index')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Создание/изменение товара'
+        return context
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+
+    def get_success_url(self):
+        return reverse('catalog:item', kwargs={'pk': self.object.pk})
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('catalog:index')
 
 
 class ContactListView(ListView):

31a9d4b8929d404a3fc32412da5cd9d583e5353f
Anna Raketa <rocketvsaliens@gmail.com>
2023-12-27 16:19:47 +0300

    Make createsuperuser custom command

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Loading env
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

f469be28cabc1cd1523a87ac783384f125735ec3
Anna Raketa <rocketvsaliens@gmail.com>
2024-01-15 18:54:32 +0300

    Homework 23.2 additional part

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import never_cache
 
 from blog.apps import BlogConfig
 from blog.views import ArticleCreateView, ArticleListView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView
@@ -7,8 +8,8 @@ app_name = BlogConfig.name
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='blog'),
-    path('create/', ArticleCreateView.as_view(), name='create_article'),
     path('view/<slug:slug>/', ArticleDetailView.as_view(), name='view_article'),
-    path('edit/<slug:slug>/', ArticleUpdateView.as_view(), name='edit_article'),
-    path('delete/<slug:slug>/', ArticleDeleteView.as_view(), name='delete_article'),
+    path('create/', never_cache(ArticleCreateView.as_view()), name='create_article'),
+    path('edit/<slug:slug>/', never_cache(ArticleUpdateView.as_view()), name='edit_article'),
+    path('delete/<slug:slug>/', never_cache(ArticleDeleteView.as_view()), name='delete_article'),
 ]
--- config/settings.py
+++ config/settings.py
@@ -56,6 +56,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.cache.UpdateCacheMiddleware",
+    "django.middleware.cache.FetchFromCacheMiddleware",
 ]
 
 ROOT_URLCONF = 'config.urls'
